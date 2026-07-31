623ce2628912e8420e91d5ba44bc8fa4444a1cfd
Noor Fatima <noor2239064@gmail.com>
2026-07-31 22:57:00 +0500

    the form is working

--- src/COMPONENTS/HOSTDASHBOARD.jsx
+++ src/COMPONENTS/HOSTDASHBOARD.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link, NavLink, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function HOSTDASHBOARD() {
+  const [ bar,setBar ] = useState(false);
+
   return (
     <>
-      <div className="flex min-h-screen">
+
+      <div className="top-bar bg-[#343434] p-6 text-[#f7f7f7]">
+        <div className="wrapper">
+          <button>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+        </div>
+      </div>
+
+
+
+      <div className="flex min-h-screen ">
         <aside className="bg-[#343434] p-6 text-[#f7f7f7]">
           <div className="flex flex-col">
             <div className="sidebar-head flex flex-col items-center">
@@ -35,7 +49,7 @@ function HOSTDASHBOARD() {
                 to={"event-pending"}
                 className={({ isActive })=> `py-2 px-3 rounded-2xl ${isActive ? "bg-[#525252] text-white" : "hover:bg-[#525252]"}`}
               >
-                Event Pending
+                Pending Approvals
               </NavLink>
               <NavLink to={"logout"} className={({ isActive })=> `py-2 px-3 rounded-2xl ${isActive ? "bg-[#525252] text-white" : "hover:bg-[#525252]"}`}>Log Out</NavLink>
             </div>
@@ -56,6 +70,10 @@ function HOSTDASHBOARD() {
 
         </main>
       </div>
+
+    
+
+
     </>
   );
 }
--- src/COMPONENTS/POSTEVENTCOMP.jsx
+++ src/COMPONENTS/POSTEVENTCOMP.jsx
@@ -1,158 +1,248 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function POSTEVENTCOMP() {
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    category: "",
+    venue: "",
+    city: "",
+    startAt: "",
+    endAt: "",
+    deadline: "",
+    capacity: "",
+    contact: "",
+    organizerName: "",
+  });
+
+  function handleEventForm(e) {
+    console.log(e.target.value);
+    const name = e.target.name;
+    const value = e.target.value;
+    setForm({ ...form, [name]: value });
+  }
+
+  async function submitForm(e) {
+    e.preventDefault();
+    // const emptyValue = Object.values(form).some((value) => value.trim() === "");
+
+    // if (emptyValue) {
+    //   alert("Please fill all fields");
+    //   return;
+    // }
+
+    Object.entries(form).forEach(([key,value])=>{
+      console.log(key, "=" ,value)
+    })
+
+    try{
+       const res = await axios.post("http://localhost:5000/api/events", form);
+    alert("Form has been submitted. Approval Pending");
+    setForm({
+      title: "",
+      description: "",
+      category: "",
+      venue: "",
+      city: "",
+      startAt: "",
+      endAt: "",
+      deadline: "",
+      capacity: "",
+      contact: "",
+      organizerName: "",
+    });
+    } catch(err) {
+      console.log("Error", err)
+    }
+    
+  }
+
   return (
     <>
-        <form action="" className="">
-          <div className="create-event wrapper bg-[#f7f7f7] rounded-3xl p-8 flex flex-col gap-4">
-            <h1 className="text-[28px] text-center text-[#343434] font-bold">
-              Add Event Details
-            </h1>
+      <form action="" className="" onSubmit={submitForm}>
+        <div className="create-event wrapper bg-[#f7f7f7] rounded-3xl p-8 flex flex-col gap-4">
+          <h1 className="text-[28px] text-center text-[#343434] font-bold">
+            Add Event Details
+          </h1>
 
-            <div className="event-form flex flex-col gap-8">
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Title:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: Hackathon"
-                />
-              </div>
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Description:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: Inviting undergraduates for a competitive hackathon"
-                />
-              </div>
+          <div className="event-form flex flex-col gap-8">
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Title:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: Hackathon"
+                value={form.title}
+                onChange={handleEventForm}
+                name="title"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Description:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: Inviting undergraduates for a competitive hackathon"
+                value={form.description}
+                onChange={handleEventForm}
+                name="description"
+              />
+            </div>
 
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Venue:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: Ideoversity"
-                />
-              </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Venue:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: Ideoversity"
+                value={form.venue}
+                onChange={handleEventForm}
+                name="venue"
+              />
+            </div>
 
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Start Time:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: 12:00 pm"
-                />
-              </div>
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  End Time:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: 3:00 pm"
-                />
-              </div>
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Last Date To Register:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="31st August, 2026."
-                />
-              </div>
-              <div className="form-item flex flex-col gap-1 italic">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Available Seats:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="200 People"
-                />
-              </div>
-              <div className="form-item flex flex-col gap-3">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Choose City:
-                </label>
-                <select
-                  name="city"
-                  id=""
-                  className="bg-white rounded-3xl p-2 focus:outline-none shadow-2xs italic"
-                >
-                  <option value="Lahore">Lahore</option>
-                  <option value="Islamabad">Islamabad</option>
-                </select>
-              </div>
-              <div className="form-item flex flex-col gap-3">
-                <label htmlFor="" className="font-medium text-[18px]">
-                  Choose Category:
-                </label>
-                <select
-                  name="category"
-                  id=""
-                  className="bg-white rounded-3xl p-2 focus:outline-none shadow-2xs italic"
-                >
-                  <option value="workshop">Workshop</option>
-                  <option value="education">Education</option>
-                  <option value="tech">Tech</option>
-                  <option value="sport">Sports</option>
-                  <option value="business">Business</option>
-                </select>
-              </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Start Time:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: 12:00 pm"
+                value={form.startAt}
+                onChange={handleEventForm}
+                name="startAt"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                End Time:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: 3:00 pm"
+                value={form.endAt}
+                onChange={handleEventForm}
+                name="endAt"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Last Date To Register:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="31st August, 2026."
+                value={form.deadline}
+                onChange={handleEventForm}
+                name="deadline"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-1 italic">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Available Seats:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="200 People"
+                value={form.capacity}
+                onChange={handleEventForm}
+                name="capacity"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-3">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Choose City:
+              </label>
+              <select
+                id=""
+                className="bg-white rounded-3xl p-2 focus:outline-none shadow-2xs italic"
+                value={form.city}
+                onChange={handleEventForm}
+                name="city"
+              >
+                <option value="">Select:</option>
+                <option value="Lahore">Lahore</option>
+                <option value="Islamabad">Islamabad</option>
+              </select>
+            </div>
+            <div className="form-item flex flex-col gap-3">
+              <label htmlFor="" className="font-medium text-[18px]">
+                Choose Category:
+              </label>
+              <select
+                id=""
+                className="bg-white rounded-3xl p-2 focus:outline-none shadow-2xs italic"
+                value={form.category}
+                onChange={handleEventForm}
+                name="category"
+              >
+                <option value="">Select:</option>
+                <option value="workshop">Workshop</option>
+                <option value="education">Education</option>
+                <option value="tech">Tech</option>
+                <option value="sport">Sports</option>
+                <option value="business">Business</option>
+              </select>
             </div>
           </div>
+        </div>
 
-          <div className=" wrapper bg-[#f7f7f7] rounded-3xl p-8 flex flex-col gap-4 mt-12">
-            <h1 className="text-[28px] text-center text-[#343434] font-bold">
-              Add Organizer's Info
-            </h1>
+        <div className=" wrapper bg-[#f7f7f7] rounded-3xl p-8 flex flex-col gap-4 mt-12">
+          <h1 className="text-[28px] text-center text-[#343434] font-bold">
+            Add Organizer's Info
+          </h1>
 
-            <div className="event-form flex flex-col gap-8">
-              <div className="form-item flex flex-col gap-3">
-                <label
-                  htmlFor=""
-                  className="font-medium font-medium text-[18px]"
-                >
-                  Organizer's Name:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="eg: Noor Fatima"
-                />
-              </div>
-              <div className="form-item flex flex-col gap-3">
-                <label
-                  htmlFor=""
-                  className="font-medium font-medium text-[18px]"
-                >
-                  Organizer's Contact:
-                </label>
-                <input
-                  type="text"
-                  className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
-                  placeholder="0123456789"
-                />
-              </div>
+          <div className="event-form flex flex-col gap-8">
+            <div className="form-item flex flex-col gap-3">
+              <label htmlFor="" className="font-medium font-medium text-[18px]">
+                Organizer's Name:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="eg: Noor Fatima"
+                value={form.organizerName}
+                onChange={handleEventForm}
+                name="organizerName"
+              />
+            </div>
+            <div className="form-item flex flex-col gap-3">
+              <label htmlFor="" className="font-medium font-medium text-[18px]">
+                Organizer's Contact:
+              </label>
+              <input
+                type="text"
+                className="bg-white rounded-3xl shadow-2xs py-2 px-4 focus:outline-none"
+                placeholder="0123456789"
+                value={form.contact}
+                onChange={handleEventForm}
+                name="contact"
+              />
             </div>
           </div>
-          <div className="wrapper mt-5 mb-15 flex justify-end">
-            <button type="Submit" className="bg-green-800 font-medium text-[#f7f7f7] py-1 px-4 rounded-3xl sm:text-[18px] text-[16px] hover:bg-green-900 cursor-pointer" >Submit For Approval</button>
-          </div>
-        </form>
-      </>
+        </div>
+        <div className="wrapper mt-5 mb-15 flex justify-end">
+          <button
+            type="Submit"
+            className="bg-green-800 font-medium text-[#f7f7f7] py-1 px-4 rounded-3xl sm:text-[18px] text-[16px] hover:bg-green-900 cursor-pointer"
+          >
+            Submit For Approval
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
